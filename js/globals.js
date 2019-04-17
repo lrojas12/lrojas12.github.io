ea389cfce845ece89ca725c71663e891856da2df
Luisa Rojas <r.luisaf@gmail.com>
2019-04-17 18:02:08 -0400

    Fixed path to PDF assets

--- js/globals.js
+++ js/globals.js
@@ -3,4 +3,4 @@ let WEBSITE_LINK = 'https://www.luisarojas.com';
 
 let IMGS_DIR_PATH = 'assets/img/';
 let ICONS_DIR_PATH = `${IMGS_DIR_PATH}icons/`;
-let PDFS_DIR_PATH = 'assets/pdf';
+let PDFS_DIR_PATH = 'assets/pdf/';
